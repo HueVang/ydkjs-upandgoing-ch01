01082bc514cea40a435a7ec7b57dc4bc25b3ac86
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2018-01-18 02:12:33 -0600

    Complete program functionality
    - Completed while loop for buying phones
    - Created alerts to display total price and apologetic alert if too
      expensive
    - Implemented calc_tax and format_price functions
    - Changed h1 tag to say 'Hello World' :)

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const ACCESSORY_PRICE = 30;
 const SPENDING_TRESHOLD = 1850;
 
 var bank_balance;
-
-bank_balance = prompt("Please enter your bank account balance: ");
+var total_price = 0;
 
 var calc_tax = (amount) => {
   return amount * TAX_RATE;
@@ -14,3 +13,16 @@ var calc_tax = (amount) => {
 var format_price = (amount) => {
   return "$" + String(amount.toFixed(2));
 }
+
+bank_balance = Number(prompt("Please enter your bank account balance: "));
+
+while (total_price < bank_balance) {
+  total_price += PHONE_PRICE;
+  if (total_price < SPENDING_TRESHOLD)
+    total_price += ACCESSORY_PRICE;
+}
+
+alert("Your total phone purchase comes out to... " + format_price(total_price + calc_tax(total_price)) + "!");
+
+if (total_price > bank_balance) 
+  alert("You don't have enough funds to buy this many phones... sorry.");
